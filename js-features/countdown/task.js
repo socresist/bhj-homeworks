634e5f4ed25e799da58db14c6c043ee5a9bd4ef7
Иван Овсянников <socresist@gmail.com>
2022-09-19 14:49:49 +0300

    corrected countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,18 +1,15 @@
-let countdownTimer = 0; 
+let countdownTimer = 0;
 
-  let x = 59; 
+let x = 59; 
 
 function countdown() {
   document.getElementById("timer").innerHTML = x;
-    x--; 
-    
-      if (x<0) {
-        clearTimeout(countdownTimer);
-          alert('Вы победили в конкурсе')
-  } 
-
-             else {
-               countdownTimer = setTimeout(countdown, 1000)
+  x--;
+  if (x<0) {
+    clearTimeout(countdownTimer);
+    alert('Вы победили в конкурсе')
+  } else {
+    countdownTimer = setTimeout(countdown, 1000)
   }
 }
 
