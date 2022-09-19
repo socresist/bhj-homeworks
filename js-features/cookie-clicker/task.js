8836fa6e541248820e30dfe46f9d6308db762c0b
Иван Овсянников <socresist@gmail.com>
2022-09-19 11:35:17 +0300

    Corrected tasks

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,23 +1,15 @@
 const image = document.getElementById("cookie");
 
-let i = 0;
 
 function changeSize() {
-  if (i==0) {
-   image.width = 250;
-   image.height = 200;
- i=1;
- } 
-  else if (i==1) {
-   image.width = 200;
-   image.height = 150;
-   i = 0;
+  return image.width = image.width === 250 ? 200 : 250
  }
- };
+
 
 const timer = document.getElementById("clicker__counter");
-
+let text = timer.textContent; 
 timer.textContent = "0"; 
+
 
 function game() {
   console.log(timer.textContent++)
@@ -26,4 +18,5 @@ function game() {
 image.addEventListener("click", function() {
   changeSize(); 
   game()
-});
+}
+);
